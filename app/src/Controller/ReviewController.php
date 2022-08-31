242bddf07cbbe67c18113cad2f86994d0697bb54
anders.bjorkland <anders.bjorkland@kreationsbyran.se>
2022-08-31 08:46:30 +0200

    Add pagination and languages

--- app/src/Controller/ReviewController.php
+++ app/src/Controller/ReviewController.php
@@ -2,30 +2,139 @@
 
 namespace App\Controller;
 
+use App\Service\GoogleBookParser;
 use GuzzleHttp\Client;
 use SilverStripe\CMS\Controllers\ContentController;
 use SilverStripe\Control\HTTPRequest;
+use SilverStripe\ORM\ArrayList;
 
 class ReviewController extends ContentController
 {
     private static $allowed_actions = [
-        'index'
+        'index',
     ];
 
     public function index(HTTPRequest $request)
     {
-        $q = $request->getVar('q');
-        $langRestriction = $request->getVar('lang') ? "&langRestrict=" . $request->getVar('lang') : "";
+        $search = $request->getVar('q');
+        $searchQuery = "q=" . $search;
 
-        if ($q) {
+        $startIndex = $request->getVar("startIndex") ?? 0;
 
-            $client = new Client();
-            $response = $client->request('GET',   'https://www.googleapis.com/books/v1/volumes?q=' . $q . $langRestriction);
-            $data = json_decode($response->getBody()->getContents(), true);
+        $langRestriction = $request->getVar("langRestrict") ?? 'any';
+        $langRestrictionQuery = $langRestriction ? "&langRestrict=" . $langRestriction : "";
 
-            return $data['items'][0]['volumeInfo']['title'];  // "Harry Potter and the Philosopher's Stone"
+        $maxResults = $request->getVar('maxResults') ?? 10;
+        $maxResultsQuery = '&maxResults=' . $maxResults;
+
+        $client = new Client();
+
+        // Get language codes
+        $response = $client->request('GET', 'https://gist.githubusercontent.com/jrnk/8eb57b065ea0b098d571/raw/936a6f652ebddbe19b1d100a60eedea3652ccca6/ISO-639-1-language.json');
+        $languageCodes = [["code" => "any", "name" => "Any"]];
+        array_push($languageCodes, ...json_decode($response->getBody()->getContents(), true));
+
+        $books = [];
+        $pagination = [];
+
+        if ($search) {
+            $basicQuery = $searchQuery
+                . $langRestrictionQuery
+                . $maxResultsQuery;
+
+            $response = $client->request('GET', 'https://www.googleapis.com/books/v1/volumes?'. $basicQuery);
+            $responseContent = json_decode($response->getBody()->getContents(), true);
+            $books = GoogleBookParser::parseAll($responseContent);
+
+            $pagination = $this->paginator('/review?' . $basicQuery, $responseContent['totalItems'], $startIndex, $maxResults);
+            $pagination['pages'] = ArrayList::create($pagination['pages']);
+            $pagination = ArrayList::create([$pagination]);
         }
 
-        return "Sorry, no valid query parameter found.";
+        return $this->customise([
+            'Layout' => $this
+                ->customise([
+                    'Books' => ArrayList::create($books),
+                    'Pagination' => $pagination,
+                    'Query' => $search,
+                    'Languages' => ArrayList::create($languageCodes),
+                    'LangRestriction' => $langRestriction
+                ])
+                ->renderWith('Layout/Books'),
+
+        ])->renderWith(['Page']);
+    }
+
+    /**
+     * Returns an array with links to pages with the necessary query parameters
+     */
+    protected function paginator($query, $count, $startIndex, $perPage): array
+    {
+        $pagination = [
+            'start' => false,
+            'current' => false,
+            'previous' => false,
+            'next' => false,
+            'totalPages' => 0,
+            'pages' => false,
+        ];
+
+        $currentPage = ceil($startIndex / $perPage) + 1;
+
+        $previousIndex = $startIndex - $perPage;
+        if ($previousIndex < 0) {
+            $previousIndex = false;
+        }
+
+        $nextIndex = $perPage * ($currentPage);
+        if ($nextIndex > $count) {
+            $nextIndex = false;
+        }
+
+        $pagination['start'] = [
+            'page' => $previousIndex > 0 ? 1 : false,
+            'link' => $previousIndex > 0 ? $query . '&startIndex=0' : false,
+        ];
+
+        $pagination['current'] = [
+            'page' => $currentPage,
+            'link' => $query . '&startIndex=' . $startIndex
+        ];
+        $pagination['previous'] = [
+            'page' => $previousIndex !== false ? $currentPage - 1 : false,
+            'link' => $previousIndex !== false ? $query . '&startIndex=' . $previousIndex : false,
+        ];
+        $pagination['next'] = [
+            'page' => $nextIndex ? $currentPage + 1 : false,
+            'link' => $nextIndex ? $query . '&startIndex=' . $nextIndex : false,
+        ];
+
+        $totalPages = ceil($count / $perPage);
+        $pagination['totalPages'] = $totalPages;
+        $pages = [];
+
+        for ($i = 0; $i < 3; $i++) {
+            $page = $currentPage + $i - 1;
+
+            if ($currentPage == 1) {
+                $page = $currentPage + $i;
+            }
+
+            if ($page > $totalPages) {
+                break;
+            }
+            if ($page < 1) {
+                continue;
+            }
+
+            $pages[] = [
+                'page' => $page,
+                'link' => $query . '&startIndex=' . ($page - 1) * $perPage,
+                'currentPage' => $page == $currentPage
+            ];
+            $pagination['pages'] = $pages;
+        }
+
+        return $pagination;
     }
 }
